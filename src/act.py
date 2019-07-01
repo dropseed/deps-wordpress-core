c53a82a932abcca16516a96bfbaeea32abc80ea6
Dave Gaeddert <dave.gaeddert@gmail.com>
2019-07-01 14:47:27 -0500

    v3

--- src/act.py
+++ src/act.py
@@ -1,44 +1,67 @@
 import os
 import json
 from subprocess import run
-
-from utils import write_json_to_temp_file
-
-
-COPY_WP_CONTENT = os.getenv('SETTING_COPY_WP_CONTENT', 'false') == 'true'
+import sys
 
 
-def act():
-    with open('/dependencies/input_data.json', 'r') as f:
+def act(input_path, output_path):
+    with open(input_path, "r") as f:
         data = json.load(f)
 
-    run(['deps', 'branch'], check=True)
+    for manifest_path, manifest_data in data.get("manifests", {}).items():
+        for dependency_name, updated_dependency_data in manifest_data["updated"][
+            "dependencies"
+        ].items():
+            assert (
+                dependency_name.lower() == "wordpress"
+            ), 'Only works on the "WordPress" dependency'
 
-    for manifest_path, manifest_data in data.get('manifests', {}).items():
-        for dependency_name, updated_dependency_data in manifest_data['updated']['dependencies'].items():
-            assert dependency_name.lower() == 'wordpress', 'Only works on the "WordPress" dependency'
+            installed = manifest_data["current"]["dependencies"][dependency_name][
+                "constraint"
+            ]
+            version_to_update_to = updated_dependency_data["constraint"]
 
-            installed = manifest_data['current']['dependencies'][dependency_name]['constraint']
-            version_to_update_to = updated_dependency_data['constraint']
-
-            version_directory = f'/tmp/wordpress/{version_to_update_to}'
-            run(f'mkdir -p {version_directory} && curl https://wordpress.org/wordpress-{version_to_update_to}.tar.gz | tar xz -C {version_directory}/', shell=True, check=True)
+            version_directory = f"/tmp/wordpress/{version_to_update_to}"
+            run(
+                f"mkdir -p {version_directory} && curl https://wordpress.org/wordpress-{version_to_update_to}.tar.gz | tar xz -C {version_directory}/",
+                shell=True,
+                check=True,
+            )
 
             def repo_wordpress_path(p):
-                return os.path.join('/repo', manifest_path, p)
+                return os.path.join(manifest_path, p)
 
-            run(['rm', '-r', repo_wordpress_path('wp-includes')], check=True)
-            run(['rm', '-r', repo_wordpress_path('wp-admin')], check=True)
-            run(['cp', '-r', os.path.join(version_directory, 'wordpress/wp-includes'), repo_wordpress_path('wp-includes')], check=True)
-            run(['cp', '-r', os.path.join(version_directory, 'wordpress/wp-admin'), repo_wordpress_path('wp-admin')], check=True)
+            run(["rm", "-r", repo_wordpress_path("wp-includes")], check=True)
+            run(["rm", "-r", repo_wordpress_path("wp-admin")], check=True)
+            run(
+                [
+                    "cp",
+                    "-r",
+                    os.path.join(version_directory, "wordpress/wp-includes"),
+                    repo_wordpress_path("wp-includes"),
+                ],
+                check=True,
+            )
+            run(
+                [
+                    "cp",
+                    "-r",
+                    os.path.join(version_directory, "wordpress/wp-admin"),
+                    repo_wordpress_path("wp-admin"),
+                ],
+                check=True,
+            )
 
-            if COPY_WP_CONTENT:
-                repo_content = repo_wordpress_path('wp-content')
-                run(f'cp -r {version_directory}/wordpress/wp-content/* {repo_content}', shell=True)
+            # if COPY_WP_CONTENT:
+            #     repo_content = repo_wordpress_path('wp-content')
+            #     run(f'cp -r {version_directory}/wordpress/wp-content/* {repo_content}', shell=True)
 
-            wordpress_root = repo_wordpress_path('.')
-            run(f'cp {version_directory}/wordpress/* {wordpress_root}', shell=True)
+            wordpress_root = repo_wordpress_path(".")
+            run(f"cp {version_directory}/wordpress/* {wordpress_root}", shell=True)
 
-            run(['deps', 'commit', '-m', 'Update {} from {} to {}'.format(dependency_name, installed, version_to_update_to), repo_wordpress_path('.')], check=True)
+    with open(output_path, "w+") as f:
+        json.dump(data, f)
 
-    run(['deps', 'pullrequest', write_json_to_temp_file(data)], check=True)
+
+if __name__ == "__main__":
+    act(sys.argv[1], sys.argv[2])
--- src/collect.py
+++ src/collect.py
@@ -2,62 +2,53 @@ import re
 from os import path
 import sys
 from subprocess import run
+import json
 
-from bs4 import BeautifulSoup
 import requests
-import semantic_version
-
-from utils import write_json_to_temp_file
 
 
-def collect():
-    run(['deps', 'hook', 'before_update'], check=True)
+def collect(input_path, output_path):
+    wordpress_path = input_path
 
-    wordpress_path = sys.argv[1]
+    version_php_path = path.join(wordpress_path, "wp-includes/version.php")
 
-    version_php_path = path.join(wordpress_path, 'wp-includes/version.php')
-
-    with open(version_php_path, 'r') as f:
+    with open(version_php_path, "r") as f:
         content = f.read()
-        found = re.search(r'^\$wp_version = \'(.*)\';$', content, re.MULTILINE)
+        found = re.search(r"^\$wp_version = \'(.*)\';$", content, re.MULTILINE)
         version_installed = found.groups()[0]
 
-    releases_html = requests.get('https://wordpress.org/download/releases/', allow_redirects=True).text
-    soup = BeautifulSoup(releases_html, 'html.parser')
-
-    available_versions = []
-
-    for tr in soup.find_all('tr'):
-        # get first td that has version number
-        td = tr.find('td')
-        version = td.text
-        available_versions.append(version)
-
-    # filter out anything below what is installed, use a list instead of set so it keeps order
-    filtered = []
-    for a in available_versions:
-        try:
-            if semantic_version.Version.coerce(a) > semantic_version.Version.coerce(version_installed):
-                if a not in filtered:
-                    filtered.append(a)
-        except ValueError:
-            # one of them is not a valid semver, it needs to be included as an option
-            if a not in filtered:
-                filtered.append(a)
+    response = requests.get("https://api.wordpress.org/core/version-check/1.7/")
+    response.raise_for_status()
+    latest = response.json()["offers"][0]["version"]
 
     schema_output = {
-        'manifests': {
+        "manifests": {
             wordpress_path: {
-                'current': {
-                    'dependencies': {
-                        'WordPress': {
-                            'constraint': version_installed,
-                            'available': [{'name': x} for x in filtered],
-                            'source': 'wordpress-core',
+                "current": {
+                    "dependencies": {
+                        "WordPress": {
+                            "constraint": version_installed,
+                            "source": "wordpress-core",
                         }
                     }
                 }
             }
         }
     }
-    run(['deps', 'collect', write_json_to_temp_file(schema_output)], check=True)
+
+    if latest != version_installed:
+        schema_output["manifests"][wordpress_path]["updated"] = {
+            "dependencies": {
+                "WordPress": {
+                    "constraint": latest,
+                    "source": "wordpress-core",
+                }
+            }
+        }
+
+    with open(output_path, "w+") as f:
+        json.dump(schema_output, f)
+
+
+if __name__ == "__main__":
+    collect(sys.argv[1], sys.argv[2])
